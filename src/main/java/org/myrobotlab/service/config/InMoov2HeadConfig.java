58e30e5ebe84e67522f0627f1c55c7c8c4ecca6c
Langevin Gael <hairygael@gmail.com>
2025-07-08 13:25:12 +0200

    removed double

--- src/main/java/org/myrobotlab/service/config/InMoov2HeadConfig.java
+++ src/main/java/org/myrobotlab/service/config/InMoov2HeadConfig.java
@@ -25,8 +25,6 @@ public class InMoov2HeadConfig extends ServiceConfig {
     
     addDefaultPeerConfig(plan, name, "eyeRightLR", "Servo", false);
     addDefaultPeerConfig(plan, name, "eyeRightUD", "Servo", false);
-    addDefaultPeerConfig(plan, name, "eyeRightLR", "Servo", false);
-    addDefaultPeerConfig(plan, name, "eyeRightUD", "Servo", false);
     addDefaultPeerConfig(plan, name, "eyebrowRight", "Servo", false);
     addDefaultPeerConfig(plan, name, "cheekRight", "Servo", false);
     
@@ -37,7 +35,6 @@ public class InMoov2HeadConfig extends ServiceConfig {
 
     addDefaultPeerConfig(plan, name, "eyeLeftLR", "Servo", false);
     addDefaultPeerConfig(plan, name, "eyeLeftUD", "Servo", false);
-
     addDefaultPeerConfig(plan, name, "eyebrowLeft", "Servo", false);
     addDefaultPeerConfig(plan, name, "cheekLeft", "Servo", false);
     
